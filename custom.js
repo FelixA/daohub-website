9c0245d656bfc2f32b5c314fb934df197e5547fa
Simon Jentzsch <simon@jentzschfamily.net>
2016-05-17 13:42:09 +0200

    formated comment

--- custom.js
+++ custom.js
@@ -1,4 +1,6 @@
-/** custom daohub js functions */
+/** 
+ * custom daohub js functions
+ */
 
 function scrollToElement(ele) {
     $(window).scrollTop(ele.offset().top).scrollLeft(ele.offset().left);
